7dbc06e7ecfef3af370bce76b6ff8c2ef95b08b4
lotrdinesh <dinesh.maheshwary@gmail.com>
2016-08-19 09:55:17 +0530

    heroku

--- cricket/settings.py
+++ cricket/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 ACCOUNT_ACTIVATION_DAYS = 7
